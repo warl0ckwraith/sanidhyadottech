48e0b91adfd0173718c963c67769efe9aa795517
Sanidhya Soni <warlockwraith07@gmail.com>
2025-05-22 04:41:53 +0530

    fixed bug speedinsights

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./components/ui/theme-provider";
