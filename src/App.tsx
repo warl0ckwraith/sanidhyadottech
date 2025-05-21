0def8587691ed37a720e8634a61c8756d40250ed
Sanidhya Soni <warlockwraith07@gmail.com>
2025-05-22 04:21:50 +0530

    Added vercel speedinsights

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./components/ui/theme-provider";
@@ -57,6 +58,7 @@ const App = () => (
         </BrowserRouter>
         <BackToTop />
         <Analytics />
+        <SpeedInsights />
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
