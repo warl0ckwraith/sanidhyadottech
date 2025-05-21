c9a0fc0f3e8ab7deb9a868741c77282b1aaf0292
Sanidhya Soni <warlockwraith07@gmail.com>
2025-05-22 04:11:45 +0530

    Added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import { ThemeProvider } from "./components/ui/theme-provider";
@@ -56,6 +56,7 @@ const App = () => (
           </Routes>
         </BrowserRouter>
         <BackToTop />
+        <Analytics />
       </TooltipProvider>
     </ThemeProvider>
   </QueryClientProvider>
